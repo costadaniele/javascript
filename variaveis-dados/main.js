d8e0c3643f124536bbe30203d5ceac5364ab6d61
Daniele <santoscostadaniele@gmail.com>
2020-10-06 01:38:10 -0300

    estudo

--- variaveis-dados/main.js
+++ variaveis-dados/main.js
@@ -6,15 +6,15 @@ var humano =  true; // Boleano
 // com colchetes
 var alunos = ['Daniele', 'Maria', 'Davi']; //Array
 
-// com ponto
+// objeto 
 var aluno = {
     nome: 'Daniele',
-    names: alunos[1],
+    names: alunos[2],
     idade: 29,
     peso: 45.5,
     humano: true
 };
 
-console.log (alunos[0]);
+console.log (alunos[1]);
 console.log(aluno.idade);
 console.log(aluno.names);
